374921a15f4eb105265cb336ee80db8f126f3689
asa_b <asa_b@LAPTOP-1PNC1425.lan>
2021-10-21 14:41:19 +0200

    testat att göra ändringar

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,11 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("Halloj!");
-<<<<<<< HEAD
 		System.out.println("Åsa här som testar");
 		
-=======
+
 		System.out.println(" Hej Ã…sa!");
->>>>>>> branch 'master' of https://github.com/AsaBoivie/GitDemoAsa.git
+
 	}
 
 }
